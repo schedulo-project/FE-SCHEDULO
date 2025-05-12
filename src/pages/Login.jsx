d6122a660a34651ba3f7357f04ac6ed6e8a9e050
Kwon <kdg5436@gmail.com>
2025-05-12 21:33:27 +0900

    design: 로그인 페이지 UI 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,31 @@
+import loginimage from "../assets/src/login/loginimage.jpg";
+import loginicon from "../assets/src/login/loginicon.svg";
+import logoimage from "../assets/src/logo/logoimage.svg";
+
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div className="flex">
+      <section className=" grow-[2] h-[100vh] border-solid border-2 border-black">
+        {/* 로고 및 서비스명 */}
+        <div className="flex gap-3">
+          <img src={logoimage} alt="" />
+          <p className="text-[26.46px]">Schedulo</p>
+        </div>
+        {/* 로그인 입력창 */}
+        <div>
+          <img src={loginicon} alt="로그인 아이콘" />
+          <p className="text-[24.08px]">로그인</p>
+        </div>
+      </section>
+      <section className="grow-[3] h-[100vh] border-solid border-2 border-black">
+        <img
+          className="w-[100%] h-[100%]"
+          src={loginimage}
+          alt="로그인 이미지"
+        />
+      </section>
+    </div>
+  );
 };
 
 export default Login;
