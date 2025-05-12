0409c0188c83ac620560499cedf723cd6cabc7a5
Kwon <kdg5436@gmail.com>
2025-05-12 16:17:00 +0900

    패키지 의존성 업데이트 및 AppRouter에서 로그인 페이지 라우팅 추가

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,12 +3,14 @@ import Home from "../pages/Home";
 import Settings from "../pages/Settings";
 import Timetable from "../pages/Timetable";
 import ChatbotComponent from "../ChatbotComponent";
+import Loginpage from "../pages/Login";
 
 // 라우팅 함수 분리
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Loginpage />} />
       <Route path="/settings" element={<Settings />} />
       <Route path="/timetable" element={<Timetable />} />
       <Route path="/chatbot" element={<ChatbotComponent />} />
