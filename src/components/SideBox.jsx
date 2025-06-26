59913d6491b846299a08ae3bffaaad147c3069ba
Kwon <kdg5436@gmail.com>
2025-06-26 09:56:07 +0900

    feat: timer page 라우팅 설정

--- src/components/SideBox.jsx
+++ src/components/SideBox.jsx
@@ -10,10 +10,12 @@ const SideBox = ({ closeSidebar }) => {
   const location = useLocation();
 
   const getSelected = () => {
-    if (location.pathname.startsWith("/timetable")) return "timetable";
+    if (location.pathname.startsWith("/timetable"))
+      return "timetable";
     if (location.pathname.startsWith("/tag")) return "tag";
-    if (location.pathname.startsWith("/clock")) return "clock";
-    if (location.pathname.startsWith("/setting")) return "setting";
+    if (location.pathname.startsWith("/timer")) return "timer";
+    if (location.pathname.startsWith("/setting"))
+      return "setting";
     if (location.pathname.startsWith("/")) return "calendar";
     return "";
   };
@@ -26,8 +28,8 @@ const SideBox = ({ closeSidebar }) => {
         nav("/");
         break;
       case "tag":
-      case "clock":
-        nav("/"); // 구현 예정
+      case "timer":
+        nav("/timer"); // 구현 예정
         break;
       case "setting":
         nav("/settings/profile");
@@ -63,7 +65,13 @@ const SideBox = ({ closeSidebar }) => {
       </section>
 
       <section className="flex flex-col items-center gap-[1.11906rem] mt-[2.41rem]">
-        {["calendar", "tag", "clock", "timetable", "setting"].map((type) => (
+        {[
+          "calendar",
+          "tag",
+          "timer",
+          "timetable",
+          "setting",
+        ].map((type) => (
           <SideBtn
             key={type}
             type={type}
