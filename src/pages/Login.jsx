4fdfc27a4131ca52642676e8eb38f557890c6ff6
Kwon <kdg5436@gmail.com>
2025-08-16 15:57:41 +0900

    fix: 로그인 및 회원가입 폼의 비밀번호 최소 길이 메시지 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -62,7 +62,7 @@ const Login = () => {
               required: "비밀번호를 입력해주세요",
               minLength: {
                 value: 4,
-                message: "비밀번호는 최소 6자 이상이어야 합니다",
+                message: "비밀번호는 최소 4자 이상이어야 합니다",
               },
             })}
           />
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -66,8 +66,8 @@ const Signup = () => {
               {...register("password", {
                 required: "비밀번호를 입력해주세요",
                 minLength: {
-                  value: 6,
-                  message: "비밀번호는 최소 6자 이상이어야 합니다",
+                  value: 4,
+                  message: "비밀번호는 최소 4자 이상이어야 합니다",
                 },
               })}
             />
