8a71c452640630cf2a91714f8f25dd58f97d977b
UHyeonj <dngusdlqwkd@gmail.com>
2025-10-31 22:01:39 +0900

    fix: 회원가입

--- src/api/Auth/signupApi.js
+++ src/api/Auth/signupApi.js
@@ -6,20 +6,36 @@ const signup = async (
   student_id,
   student_password
 ) => {
-  try {
-    const response = await baseAxiosInstance.post(
-      "/users/signup/",
-      {
-        email,
-        password,
-        student_id,
-        student_password,
-      }
-    );
-    return { data: response.data, success: true };
-  } catch (error) {
-    console.error("회원가입 실패:", error);
-    throw error;
+  if (student_id == null || student_password == null) {
+    try {
+      const response = await baseAxiosInstance.post(
+        "/users/signup/",
+        {
+          email,
+          password,
+        }
+      );
+      return { data: response.data, success: true };
+    } catch (error) {
+      console.error("회원가입 실패:", error);
+      throw error;
+    }
+  } else {
+    try {
+      const response = await baseAxiosInstance.post(
+        "/users/signup/",
+        {
+          email,
+          password,
+          student_id,
+          student_password,
+        }
+      );
+      return { data: response.data, success: true };
+    } catch (error) {
+      console.error("회원가입 실패:", error);
+      throw error;
+    }
   }
 };
 
